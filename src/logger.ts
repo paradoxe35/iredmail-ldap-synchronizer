43a55c623400d95c64af61e2314140892553b937
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2022-05-30 15:44:10 +0200

    enable pm2 process ansible

--- src/logger.ts
+++ src/logger.ts
@@ -1,4 +1,5 @@
 import { createLogger, format, transports } from "winston";
+import send_mail from "./notifications";
 
 const logLevels = {
   fatal: 0,
@@ -13,4 +14,9 @@ export const Logger = createLogger({
   levels: logLevels,
   format: format.combine(format.timestamp(), format.json()),
   transports: [new transports.Console()],
+});
+
+Logger.on("error", (err) => {
+  err.message = `${err.message} ${err.stack}`;
+  send_mail({ subject: "Logger Error", message: err.message });
 });
